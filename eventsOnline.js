4921705cd4a14b77552a2099df0553401fbf949e
W112591 <cedric.bouquet@worldline.com>
2021-07-23 15:58:51 +0200

    :memo: update eventsOnline

---
+++ eventsOnline.js
@@ -0,0 +1,5 @@
+a2021 = new Annee(2021);
+a2021.addEvent(28, "Juillet", "Super Event");
+a2021.addTache(30, "Juillet", "Faire tâche");
+a2021.addJourney("0107","0209","Vacances");
+a2021.affiche("Juillet", "Novembre");
